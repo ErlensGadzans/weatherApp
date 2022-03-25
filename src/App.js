4b862cc397d653a074ae6a6a0fa49f85861d3ed4
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-03-26 01:09:57 +0200

    Receiving data from api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
+
   return (
     <div className="app">
+      <div className="searchBar">
+        <input
+          type="text"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Choose the city"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
